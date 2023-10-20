808eeb51443bc3280afe58e4475f94158589f391
nisha1289 <147962898+nisha1289@users.noreply.github.com>
2023-10-20 20:58:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,31 +21,13 @@
 #  SOFTWARE
 
 
-import requests
-import json
-import subprocess
-from pyrogram.types.messages_and_media import message
-import helper
-from pyromod import listen
-from pyrogram.types import Message
-import tgcrypto
-import pyrogram
-from pyrogram import Client, filters
-from pyrogram.types.messages_and_media import message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-import asyncio
-from pyrogram.errors import FloodWait
-import time
-from pyrogram.types import User, Message
-from p_bar import progress_bar
-import subprocess
-from subprocess import getstatusoutput
-import logging
 import os
-import sys
-from get_video_info import get_video_attributes, get_video_thumb
-import re
-from pyrogram import Client as bot
+import config
+from config import Config
+from pyrogram import Client, idle
+import asyncio, logging
+import tgcrypto
+from pyromod import listen
 from logging.handlers import RotatingFileHandler
 
 LOGGER = logging.getLogger(__name__)
